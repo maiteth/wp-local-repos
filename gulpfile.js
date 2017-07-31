25aea80f1300a2685f5589255310a6565900a8a1
Maïté THOMIAS <maite.thomias@gmail.com>
2017-07-31 15:03:16 +0200

    add: gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,29 @@ const path = {
 	undeploy: 'lib/deploy/remove.php',
 };
 
+gulp.task('config:htaccess', function () {
+	const devEnv = cfgUtils.getEnv('dev');
+	return gulp.src(path.htaccess)
+		.pipe(ejs(devEnv.htaccess))
+		.pipe(rename('.htaccess'))
+		.pipe(gulp.dest(path.base));
+});
+
+gulp.task('config:wp-config', function () {
+	const devEnv = cfgUtils.getEnv('dev');	
+	return gulp.src(path.wpConfig)
+		.pipe(ejs(devEnv.mysql))
+		.pipe(rename('wp-config.php'))
+		.pipe(gulp.dest(path.base));
+});
+
+gulp.task('config:db-import', function (cb) {
+	require('./utils/db-import.js');
+	cb();
+});
+
+gulp.task('config', ['config:htaccess', 'config:wp-config', 'config:db-import']);
+
 
 // Delete the dist directory
 gulp.task('clean:dist', function () {
@@ -49,6 +72,11 @@ gulp.task('clean:zip', function () {
 });
 
 gulp.task('clean', ['clean:dist', 'clean:zip']);
+
+
+
+
+
 
 gulp.task('resources', function () {
 	return gulp.src(path.resources, {
@@ -87,7 +115,7 @@ gulp.task('sql', function (cb) {
 	console.log('prefix', prefix);
 	const localUrl = 'http://localhost/wp-local-repos/app';
 	const deployUrl = deployEnv.url;
-	const regexp = new RegExp(`${localUrl}`,'g');
+	const regexp = new RegExp(`${localUrl}`, 'g');
 	return gulp.src(path.sql)
 		.pipe(replace(/^(.*(?:TABLE|table|Table|INSERT INTO).*?)wp_(.*)$/mg, `$1${prefix}$2`))
 		.pipe(replace(/^(\(\d+,\d+,')wp_(.*)$/mg, `$1${prefix}$2`))
@@ -104,6 +132,11 @@ gulp.task('build', function () {
 gulp.task('rebuild', function () {
 	runSequence('clean', 'build');
 });
+
+
+
+
+
 
 gulp.task('deploy:zip', function (callback) {
 	return gulp.src(path.zipSrc, {
@@ -135,15 +168,15 @@ gulp.task('deploy:unzip', function (callback) {
 		});
 });
 
-
-
-
-
 gulp.task('deploy', ['clean:zip'], function () {
-	runSequence('deploy:zip', 'deploy:ftp', 'deploy:unzip' );
+	runSequence('deploy:zip', 'deploy:ftp', 'deploy:unzip');
 });
 
-gulp.task('undeploy:ftp', function() {
+
+
+
+
+gulp.task('undeploy:ftp', function () {
 	const deployEnv = cfgUtils.getEnv('deploy');
 	console.log('env.ftp', deployEnv.ftp);
 	return gulp.src(path.undeploy)
@@ -151,19 +184,19 @@ gulp.task('undeploy:ftp', function() {
 		.pipe(gutil.noop());
 });
 
-gulp.task('undeploy:remove', function(callback) {
+gulp.task('undeploy:remove', function (callback) {
 	const deployEnv = cfgUtils.getEnv('deploy');
 	rp(deployEnv.url + '/remove.php')
-		.then(function(htmlString) {
+		.then(function (htmlString) {
 			console.log('htmlString', htmlString);
 			callback();
 		})
-		.catch(function(err) {
+		.catch(function (err) {
 			console.log('error', err);
 			throw err;
 		});
 });
 
-gulp.task('undeploy', function() {
+gulp.task('undeploy', function () {
 	runSequence('undeploy:ftp', 'undeploy:remove');
 });

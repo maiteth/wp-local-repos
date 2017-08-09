308a0105d0883664a89c4772d2bfd8a0efb99023
Maïté THOMIAS <maite.thomias@gmail.com>
2017-08-09 10:34:05 +0200

    refactor: gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -28,29 +28,7 @@ const path = {
 };
 
 require('./gulp/clean.js')(gulp, path);
-
-gulp.task('config:htaccess', function () {
-	const devEnv = cfgUtils.getEnv('dev');
-	return gulp.src(path.htaccess)
-		.pipe(ejs(devEnv.htaccess))
-		.pipe(rename('.htaccess'))
-		.pipe(gulp.dest(path.base));
-});
-
-gulp.task('config:wp-config', function () {
-	const devEnv = cfgUtils.getEnv('dev');
-	return gulp.src(path.wpConfig)
-		.pipe(ejs(devEnv.mysql))
-		.pipe(rename('wp-config.php'))
-		.pipe(gulp.dest(path.base));
-});
-
-gulp.task('config:db-import', function (cb) {
-	require('./utils/db-import.js');
-	cb();
-});
-
-gulp.task('config', ['config:htaccess', 'config:wp-config', 'config:db-import']);
+require('./gulp/config.js')(gulp, path);
 
 
 

0d07158405e76ab72af98aeed8e1165667d69340
Maïté THOMIAS <maite.thomias@gmail.com>
2017-08-09 15:57:24 +0200

    add: gulp clean

--- gulp/clean.js
+++ gulp/clean.js
@@ -2,13 +2,9 @@ const del = require('del');
 
 module.exports = function (gulp, path) {
 	// Delete the dist directory
-	gulp.task('clean:dist', function () {
-		return del([path.dist]);
+	gulp.task('clean:dist', function (cb) {
+		cb();
 	});
 
-	gulp.task('clean:zip', function () {
-		return del([path.zip]);
-	});
-
-	gulp.task('clean', ['clean:dist', 'clean:zip']);
+	gulp.task('clean', ['clean:dist']);
 };

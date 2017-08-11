3f96f56e158e09a2a46cd33f1948c4dab0199c03
Maïté THOMIAS <maite.thomias@gmail.com>
2017-08-11 09:01:49 +0200

    add: merge

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -4,7 +4,6 @@ const spawn = require('child_process').spawn;
 const deployEnv = cfgUtils.getEnv('deploy');
 
 module.exports = function(gulp, pathConfig) {
-<<<<<<< HEAD
 	gulp.task('deploy:push', function(cb) {
 		function init() {
 			const init = spawn('git', [
@@ -30,35 +29,6 @@ module.exports = function(gulp, pathConfig) {
 				console.log('init failed.', err);
 			});
 		}
-=======
-
-	function init() {
-		const init = spawn('git', [
-			'ftp',
-			'init',
-			'-u',
-			deployEnv.ftp.user,
-			'-p', deployEnv.ftp.pass,
-			`${deployEnv.ftp.host}/${deployEnv.ftp.remotePath}`
-		], { cwd: pathConfig.dist });
-		init.stdout.on('data', (data) => {
-			console.log(`stdout: ${data}`);
-		});
-
-		init.stderr.on('data', (data) => {
-			console.log(`stderr: ${data}`);
-		});
-		init.on('close', (code) => {
-			console.log(`git init exited with code ${code}`);
-		});
-
-		init.on('error', (err) => {
-			console.log('init failed.', err);
-		});
-	}
-
-	gulp.task('deploy:push', function(cb) {
->>>>>>> 59d965ae1763b8590a5b14c62b1f3fa2cb1e17ee
 
 		const push = spawn('git', [
 			'ftp',
